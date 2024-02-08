a39e290434ee25e6500641243f9f321224b9178b
adinareiter <144619417+adinareiter@users.noreply.github.com>
2024-02-07 19:50:11 -0500

    deletes the filtering logic code for mapping through menu

--- src/MenusShow.jsx
+++ src/MenusShow.jsx
@@ -5,8 +5,6 @@ import axios from "axios";
 
 export function MenusShow(props) {
   const params = useParams();
-  // const [menu, setMenu] = useState(null);
-  // const [menusWithSameEventName, setMenusWithSameEventName] = useState([]);
   const [eventMenu, setEventMenu] = useState({});
 
   useEffect(() => {
@@ -20,40 +18,7 @@ export function MenusShow(props) {
         console.error("Error fetching event data:", error);
         setEventMenu(null); // Set menu to null in case of an error
       });
-    // Fetch the menu data using params
-    // axios
-    //   .get(`http://localhost:3000/menus/${params.menuId}.json`)
-    //   .then((response) => {
-    //     setMenu(response.data);
-    //     console.log("Fetched menu:", response.data);
-
-    //     // Once you have the 'event' from the specific menu, fetch menus with the same event name
-    //     axios
-    //       .get(`http://localhost:3000/menus.json`)
-    //       .then((menusResponse) => {
-    //         const menusWithSameEvent = menusResponse.data.filter(
-    //           (menu) => menu.event.title === response.data.event.title
-    //         );
-    //         setMenusWithSameEventName(menusWithSameEvent);
-    //         console.log("Menus with the same event name:", menusWithSameEvent);
-    //       })
-    //       .catch((menusError) => {
-    //         console.error("Error fetching menus data:", menusError);
-    //         setMenusWithSameEventName([]); // Set to an empty array in case of an error
-    //       });
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error fetching menu data:", error);
-    //     setMenu(null); // Set menu to null in case of an error
-    //   });
   }, [params.menuId]); // Trigger the effect whenever menuId changes
-
-  // if (!eventMenu || !eventMenu.recipes.length === 0) {
-  //   console.log(" menuId", params.menuId);
-  //   // Handle the case where the data is not available
-  //   return <div>No data available</div>;
-  // }
-  // const { event, recipe } = menu;
 
   return (
     <div>
