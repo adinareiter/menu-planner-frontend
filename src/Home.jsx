9eb365886a19ea3335950009476897681f1cbaf0
adinareiter <144619417+adinareiter@users.noreply.github.com>
2024-02-14 17:01:42 -0500

    adds recipes and menus link on home page appearing after welcome

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,47 @@
+import React, { useState, useEffect } from "react";
 import Typewriter from "./Typewriter";
+import { Link } from "react-router-dom";
 
 export function Home() {
+  const [complete, setComplete] = useState(false);
+
+  const handleComplete = () => {
+    setComplete(true);
+  };
+
+  useEffect(() => {
+    // Set a timeout for the "Recipes" and "Menus" links
+    const timeout = setTimeout(() => {
+      handleComplete();
+    }, 100 * 32);
+
+    // Clean up the timeout when the component unmounts or when both links appear
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
   return (
     <div id="home">
-      <h1>
-        <Typewriter text="  Welcome to my Menu Planner" delay={100} />
-      </h1>
+      <div id="welcome">
+        <h1>
+          <Typewriter text="   Welcome to my Menu Planner" delay={100} onComplete={handleComplete} />
+        </h1>
+      </div>
+      {complete && (
+        <div id="links-container">
+          <h5 id="recipes">
+            <Link to="/recipes" className="nav-link active" aria-current="page">
+              Recipes
+            </Link>
+          </h5>
+          <h5 id="menus">
+            <Link to="/menus" className="nav-link active" aria-current="page">
+              Menus
+            </Link>
+          </h5>
+        </div>
+      )}
     </div>
   );
 }
